fe916035962910d2b5d4baf02200332b1fc095b4
Ville Selkämaa <ville.selkamaa97@hotmail.com>
2023-02-19 20:50:54 +0200

    Tailwind fix attempt

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-
 # This file is ran after devcontainer is created.
-
+pip install pytailwindcss
 # Copy kube config to the container
 echo "source $PWD/.devcontainer/copy-kube-config.sh" | tee -a $HOME/.bashrc $HOME/.zshrc >> /dev/null
